e7d60b453295f76a341d04a96b285d9d613668ad
dhanyn10 <dhanyn.dhan@gmail.com>
2022-02-14 09:39:29 +0700

    tabs -> spasi

--- app.go
+++ app.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+    c1 := make(chan string)
+    c2 := make(chan string)
 
-	go func() {
-		for {
-			c1 <- "500ms"
-			time.Sleep(time.Millisecond * 500)
-		}
-	}()
-	go func() {
-		for {
-			c1 <- "2s"
-			time.Sleep(time.Second * 2)
-		}
-	}()
+    go func() {
+        for {
+            c1 <- "500ms"
+            time.Sleep(time.Millisecond * 500)
+        }
+    }()
+    go func() {
+        for {
+            c1 <- "2s"
+            time.Sleep(time.Second * 2)
+        }
+    }()
 
-	for i := 1; i < 20; i++{
-		select {
-		case msg1 := <- c1:
-			fmt.Println(msg1)
-		case msg2 := <- c2:
-			fmt.Println(msg2)
-		}
-	}
+    for i := 1; i < 20; i++{
+        select {
+        case msg1 := <- c1:
+            fmt.Println(msg1)
+        case msg2 := <- c2:
+            fmt.Println(msg2)
+        }
+    }
 }
