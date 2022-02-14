eee883fcb54a34afed78b79d0362a02b6bb750c6
dhanyn10 <dhanyn.dhan@gmail.com>
2022-02-14 09:36:38 +0700

    tabs

--- app.go
+++ app.go
@@ -8,26 +8,26 @@ import (
 func main() {
 	c1 := make(chan string)
 	c2 := make(chan string)
-	
+
 	go func() {
-	  for {
-	    c1 <- "500ms"
-	    time.Sleep(time.Millisecond * 500)
-	  }
+		for {
+			c1 <- "500ms"
+			time.Sleep(time.Millisecond * 500)
+		}
 	}()
 	go func() {
-	  for {
-	    c1 <- "2s"
-	    time.Sleep(time.Second * 2)
-	  }
+		for {
+			c1 <- "2s"
+			time.Sleep(time.Second * 2)
+		}
 	}()
-	
+
 	for i := 1; i < 20; i++{
-	  select {
-	    case msg1 := <- c1:
-	      fmt.Println(msg1)
-	    case msg2 := <- c2:
-	      fmt.Println(msg2)
-	  }
+		select {
+		case msg1 := <- c1:
+			fmt.Println(msg1)
+		case msg2 := <- c2:
+			fmt.Println(msg2)
+		}
 	}
 }
